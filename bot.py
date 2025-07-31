c294239226b072c5ba2897ed18dec507aafe4dd9
Ilya Vostrikov <ilyavostrikov90@gmail.com>
2025-07-31 11:03:35 +0300

    Добавил подключение classic_menu.router

--- bot.py
+++ bot.py
@@ -1,13 +1,24 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram import Router
 import asyncio
+
+from handlers import classic_menu
 
 # Токен Telegram-бота
 BOT_TOKEN = "8225464968:AAGzQO0JqRGVwxMczrHBancRLxDRdXfgV_I"
 
+# Создаем бота и диспетчер
 bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(storage=MemoryStorage())
 
+# Создаем роутер и подключаем обработчики
+app = Router()
+app.include_router(classic_menu.router)
+dp.include_router(app)
+
+# Хендлер для старта
 @dp.message()
 async def start(message: types.Message):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
@@ -18,11 +29,10 @@ async def start(message: types.Message):
     ])
     await message.answer("Привет! Нажми кнопку ниже, чтобы открыть меню ☕", reply_markup=keyboard)
 
+# Точка входа
 async def main():
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
-    asyncio.run(main())
 if __name__ == "__main__":
     try:
         asyncio.run(main())
